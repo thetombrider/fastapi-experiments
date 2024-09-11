17573179a8077ccacdbdbf4ed7b998de2ca79d21
tommy <tommasominuto@gmail.com>
2024-09-11 18:51:22 +0200

    added middleware to use on render

--- library_mgmt_api/main.py
+++ library_mgmt_api/main.py
@@ -2,11 +2,21 @@
 from fastapi import FastAPI, Depends
 from routes import router
 from dependencies import get_supabase
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Include the router and pass the Supabase client
 app.include_router(router, dependencies=[Depends(get_supabase)])
+
+# Implement CORS to control allowed origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://library-40ni.onrender.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
